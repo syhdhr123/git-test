a1afc55bbab75fc222d9ac208192909bb2012349
shayiheng <1355978682@qq.com>
2019-05-11 14:35:38 +0800

    把本地工程添加到本地

---
+++ git-test-project/src/com/sha/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.sha.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
